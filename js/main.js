536e399b4c800c06d4360b663d3e348c6165844f
BeanBeam <s110819015@stu.ntue.edu.tw>
2021-04-22 01:06:42 +0800

    add some json

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
-let pageURL = "./json/110819015.json";
+let serverURL = './json/110819015.json';
 
-$(document).ready(function(){
-    $.get(pageURL, function(data){
-        console.log(data);
-    });
+$(document).ready(function() {
+	readFromServer();
 });
-function setImg(){
-    for(let i=0; i<imgAry.length; i++){
-        let tmp = $('#template01');
-        let htmlCode = tmp.html(); //括號裡面沒放東西，是取內容出來
-        htmlCode = htmlCode.replace('IMG_HERE',imgAry[i]);
-        htmlCode = htmlCode.replace('TEXT_HERE',textAry[i]);
-        $('body').append(htmlCode);
-    }
+
+function readFromServer(){
+	var parameter = {};
+	$.get(serverURL, function(data) {
+		console.log(data);
+		init(data);
+	});
+}
+
+function init(data){
+	// var htmlCode = '<img src="'+data.imgurl+'">';
+	// $('body').append(htmlCode)
+
+	$('.img1').css('background-image','url(' + data.imgurl + ')');
+    $('.line1').html(data.name);
+    $('.line2').html(data.photoNum + '張相片・' + data.viewNum + '次檢視');
 }
