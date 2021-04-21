a14da5bc09953255b26b84c0b839b523c53485fc
BeanBeam <s110819015@stu.ntue.edu.tw>
2021-04-22 02:38:29 +0800

    add box okay

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
-let serverURL = './json/110819015.json';
+// let serverURL = './json/110819015.json';
+let serverURL = [
+    './json/110719016.json',
+    './json/110719018.json',
+    './json/110719032.json',
+    './json/110719033.json',
+    './json/110719042.json',
+    './json/110819015.json',
+    './json/110819023.json'
+];
+// let serverURL = [];
+// serverURL.push('./json/110719016.json');
 
 $(document).ready(function() {
 	readFromServer();
@@ -6,17 +17,33 @@ $(document).ready(function() {
 
 function readFromServer(){
 	var parameter = {};
-	$.get(serverURL, function(data) {
-		console.log(data);
-		init(data);
-	});
+    for(let i=0; i<serverURL.length; i++){
+        $.get(serverURL[i], function(data) {
+            console.log(data);
+            init(data);
+        });
+    }
 }
 
 function init(data){
 	// var htmlCode = '<img src="'+data.imgurl+'">';
 	// $('body').append(htmlCode)
 
-	$('.img1').css('background-image','url(' + data.imgurl + ')');
-    $('.line1').html(data.name);
-    $('.line2').html(data.photoNum + '張相片・' + data.viewNum + '次檢視');
+    // for(let i=0; i<serverURL.length; i++){
+        
+    // }
+    // $('.img1').css('background-image','url(' + data.imgurl + ')');
+    // $('.line1').html(data.name);
+    // $('.line2').html(data.photoNum + '張相片・' + data.viewNum + '次檢視');
+	
+    
+    let tmp = $('#template01');
+    let htmlCode = tmp.html(); //括號裡面沒放東西，是取內容出來
+    $('.img1').css('background-image','url('+ data.imgurl +')');
+    htmlCode = htmlCode.replace('NAME_HERE',data.name);
+    htmlCode = htmlCode.replace('PHOTONUM_HERE',data.photoNum);
+    htmlCode = htmlCode.replace('VIEWNUM_HERE',data.viewNum);
+    
+    $('section .row').append(htmlCode);
+    
 }
