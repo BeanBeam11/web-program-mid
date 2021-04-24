0b87ef19f25afc1a5c18f0f34d39081789a420e3
BeanBeam <s110819015@stu.ntue.edu.tw>
2021-04-25 00:55:10 +0800

    remove useless code

--- js/main.js
+++ js/main.js
@@ -1,54 +1,3 @@
-// // let serverURL = './json/110819015.json';
-// let serverURL = [
-//     './json/110619049.json',
-//     './json/110719016.json',
-//     './json/110719018.json',
-//     './json/110719032.json',
-//     './json/110719033.json',
-//     './json/110719042.json',
-//     './json/110819013.json',
-//     './json/110819015.json',
-//     './json/110819023.json',
-//     './json/110819042.json'
-// ];
-// let img_NUM = '';
-// let album_LINK ='';
-// let url ='';
-
-// $(document).ready(function() {
-// 	readFromServer();
-// });
-
-// function readFromServer(){
-// 	var parameter = {};
-//     for(let i=0; i<serverURL.length; i++){
-//         $.get(serverURL[i], function(data) {
-//             console.log(data);
-//             img_NUM = 'img' + i;
-//             $('.img').toggleClass(img_NUM);
-//             // $('.' + img_NUM).css('background-image','url('+ data.imgurl +')');
-//             init(data);
-//             $('.img').toggleClass(img_NUM);
-
-//             $('.' + img_NUM).css('background-image','url('+ data.imgurl +')');
-//         });
-//     }
-// }
-
-// function init(data){
-
-//     let tmp = $('#template01');
-//     let htmlCode = tmp.html(); //括號裡面沒放東西，是取內容出來
-    
-//     htmlCode = htmlCode.replace('NAME_HERE',data.name);
-//     htmlCode = htmlCode.replace('PHOTONUM_HERE',data.photoNum);
-//     htmlCode = htmlCode.replace('VIEWNUM_HERE',data.viewNum);
-    
-//     $('section .row').append(htmlCode);
-// }
-
-
-// let serverURL = './json/110819015.json';
 let serverURL = '../json/all_data.json';
 let img_NUM = '';
 let box_NUM = '';
@@ -64,7 +13,6 @@ function readFromServer(){
     $.get(serverURL, function(data) {
         console.log(data);
         init(data);
-        log(data);
     });
     
 }
@@ -75,9 +23,6 @@ function init(data){
     let htmlCode = tmp.html(); //括號裡面沒放東西，是取內容出來
 
     for(let i =0; i < data.length; i++){
-        // htmlCode = htmlCode.replace('NAME_HERE',data[i].name);
-        // htmlCode = htmlCode.replace('PHOTONUM_HERE',data[i].photoNum);
-        // htmlCode = htmlCode.replace('VIEWNUM_HERE',data[i].viewNum);
         img_NUM = 'img' + i;
         box_NUM = 'box' + i;
         $('.img').toggleClass(img_NUM);
