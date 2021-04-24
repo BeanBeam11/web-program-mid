bea4bd2eeabfef0eb51d1e1b199693ff8bb7a9a9
BeanBeam <s110819015@stu.ntue.edu.tw>
2021-04-25 00:51:20 +0800

    finish index

--- js/main.js
+++ js/main.js
@@ -51,6 +51,7 @@
 // let serverURL = './json/110819015.json';
 let serverURL = '../json/all_data.json';
 let img_NUM = '';
+let box_NUM = '';
 let album_LINK ='';
 let url ='';
 
@@ -62,7 +63,8 @@ function readFromServer(){
 	var parameter = {};
     $.get(serverURL, function(data) {
         console.log(data);
-        // log(data);
+        init(data);
+        log(data);
     });
     
 }
@@ -71,19 +73,28 @@ function init(data){
 
     let tmp = $('#template01');
     let htmlCode = tmp.html(); //括號裡面沒放東西，是取內容出來
+
     for(let i =0; i < data.length; i++){
-        htmlCode = htmlCode.replace('NAME_HERE',data.name);
-        htmlCode = htmlCode.replace('PHOTONUM_HERE',data.photoNum);
-        htmlCode = htmlCode.replace('VIEWNUM_HERE',data.viewNum);
+        // htmlCode = htmlCode.replace('NAME_HERE',data[i].name);
+        // htmlCode = htmlCode.replace('PHOTONUM_HERE',data[i].photoNum);
+        // htmlCode = htmlCode.replace('VIEWNUM_HERE',data[i].viewNum);
         img_NUM = 'img' + i;
+        box_NUM = 'box' + i;
         $('.img').toggleClass(img_NUM);
-        $('.' + img_NUM).css('background-image','url('+ data.imgurl +')');
-        $('.img').toggleClass(img_NUM);
-        
+        $('.box').toggleClass(box_NUM);
+
         $('section .row').append(htmlCode);
+
+        $('.img').toggleClass(img_NUM);
+        $('.box').toggleClass(box_NUM);
+        $('.' + img_NUM).css('background-image','url('+ data[i].imgurl +')');
+        $('.' + box_NUM + ' .line1').html(data[i].name);
+        $('.' + box_NUM + ' .line2').html(data[i].photoNum + '張相片・' + data[i].viewNum + '次檢視');
+
+        console.log(data[i].name);
     }
 }
 
-function log(data){
-    console.log(data[0]);
+function log(data){ //列印json陣列->檢查
+    console.log(data[2].imgurl);
 }
