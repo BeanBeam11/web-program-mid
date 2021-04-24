f7231db21ebc3f5d46eed2dea525de444ee37f9f
BeanBeam <s110819015@stu.ntue.edu.tw>
2021-04-24 22:45:42 +0800

    add data to all_data.json

--- js/main.js
+++ js/main.js
@@ -24,17 +24,12 @@
 //     for(let i=0; i<serverURL.length; i++){
 //         $.get(serverURL[i], function(data) {
 //             console.log(data);
-//             album_LINK = 'album-link' + i;
 //             img_NUM = 'img' + i;
-//             url = "./album.html?" + serverURL[i];
-//             $('.album-link').toggleClass(album_LINK);
 //             $('.img').toggleClass(img_NUM);
 //             // $('.' + img_NUM).css('background-image','url('+ data.imgurl +')');
 //             init(data);
-//             $('.album-link').toggleClass(album_LINK);
 //             $('.img').toggleClass(img_NUM);
 
-//             $('.album-link').attr("href",url);
 //             $('.' + img_NUM).css('background-image','url('+ data.imgurl +')');
 //         });
 //     }
@@ -54,23 +49,10 @@
 
 
 // let serverURL = './json/110819015.json';
-let serverURL = [
-    './json/110619049.json',
-    './json/110719016.json',
-    './json/110719018.json',
-    './json/110719032.json',
-    './json/110719033.json',
-    './json/110719042.json',
-    './json/110819013.json',
-    './json/110819015.json',
-    './json/110819023.json',
-    './json/110819042.json'
-];
+let serverURL = '../json/all_data.json';
 let img_NUM = '';
 let album_LINK ='';
 let url ='';
-let jsonAry = [];
-let jsonURL ='';
 
 $(document).ready(function() {
 	readFromServer();
@@ -78,13 +60,10 @@ $(document).ready(function() {
 
 function readFromServer(){
 	var parameter = {};
-    for(let i=0; i<serverURL.length; i++){
-        jsonURL = serverURL[i];
-        $.get(jsonURL, function(data) {
-            // console.log(data);
-            jsonAry.push(data);
-        });
-    }
+    $.get(serverURL, function(data) {
+        console.log(data);
+        // log(data);
+    });
     
 }
 
@@ -92,10 +71,19 @@ function init(data){
 
     let tmp = $('#template01');
     let htmlCode = tmp.html(); //括號裡面沒放東西，是取內容出來
-    
-    htmlCode = htmlCode.replace('NAME_HERE',data.name);
-    htmlCode = htmlCode.replace('PHOTONUM_HERE',data.photoNum);
-    htmlCode = htmlCode.replace('VIEWNUM_HERE',data.viewNum);
-    
-    $('section .row').append(htmlCode);
+    for(let i =0; i < data.length; i++){
+        htmlCode = htmlCode.replace('NAME_HERE',data.name);
+        htmlCode = htmlCode.replace('PHOTONUM_HERE',data.photoNum);
+        htmlCode = htmlCode.replace('VIEWNUM_HERE',data.viewNum);
+        img_NUM = 'img' + i;
+        $('.img').toggleClass(img_NUM);
+        $('.' + img_NUM).css('background-image','url('+ data.imgurl +')');
+        $('.img').toggleClass(img_NUM);
+        
+        $('section .row').append(htmlCode);
+    }
+}
+
+function log(data){
+    console.log(data[0]);
 }
