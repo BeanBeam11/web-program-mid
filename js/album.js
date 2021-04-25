fb230212dedc66b2ac24333086425c16a0d1ee5b
BeanBeam <s110819015@stu.ntue.edu.tw>
2021-04-25 13:04:16 +0800

    almost finish album row (I'm smart cookie)

--- js/album.js
+++ js/album.js
@@ -1,9 +1,12 @@
 let serverURL = '../json/all_data.json';
-let album_link_NUM ='';
+//抓json的ID
 let urlStr = document.location.toString();
 let urlID = urlStr.split('?')[1];
 console.log(urlID);
-let urlNUM = '';
+
+let urlNUM = '';//確認為哪一個json
+
+let img_photo_NUM = '';
 
 $(document).ready(function() {
 	readFromServer();
@@ -22,19 +25,19 @@ function init(data){
     let tmp = $('#template02');
     let htmlCode = tmp.html(); //括號裡面沒放東西，是取內容出來
 
-    for(let i =0; i < data.length; i++){ 
+    for(let i =0; i < data.length; i++){ //確認為哪一個json
         if( data[i].ID == urlID ){
             urlNUM = i;
         }
     }
-
+    //設定img-cover
     $('.img-cover').css('background-image','url('+ data[urlNUM].imgurl +')');
     $('.album-name').html(data[urlNUM].name);
     $('.album-info').html(data[urlNUM].info);
     $('.album-num').html(data[urlNUM].photoNum + '張相片・' + data[urlNUM].viewNum + '次檢視');
     $('.album-autor-img').css('background-image','url('+ data[urlNUM].authorImg +')');
     $('.album-author-name').html(data[urlNUM].author);
-
+    //設定img-photo
     $('.img-photo').css('background-image','url('+ data[urlNUM].images[0].imgurl +')');
     $('.photo-title').html(data[urlNUM].images[0].title);
     $('.photo-author').html(data[urlNUM].images[0].author);
@@ -45,6 +48,22 @@ function init(data){
         
     //     $('section .row').append(htmlCode);
     // }
+    for(let i =0; i<data[urlNUM].images.length; i++){ //確認有幾張相片
+        img_photo_NUM = 'img-photo-num' + i;
+        $('.img-photo-num').toggleClass(img_photo_NUM);
+
+        $('section .row').append(htmlCode);
+
+        $('.img-photo-num').toggleClass(img_photo_NUM);
+
+        $('.' + img_photo_NUM + ' .img-photo').css('background-image','url('+ data[urlNUM].images[i].imgurl +')');
+        $('.' + img_photo_NUM + ' .photo-title').html(data[urlNUM].images[i].title);
+        $('.' + img_photo_NUM + ' .photo-author').html(data[urlNUM].images[i].author);
+        $('.' + img_photo_NUM + ' .photo-like-num').html('<i class="fa fa-star-o" aria-hidden="true"></i> ' + data[urlNUM].images[i].likeNum);
+        $('.' + img_photo_NUM + ' .photo-comment-num').html('<i class="fa fa-comment-o" aria-hidden="true"></i> ' + data[urlNUM].images[i].commentNum);
+
+        
+    }
 }
 
 function log(data){ //列印json陣列->檢查
